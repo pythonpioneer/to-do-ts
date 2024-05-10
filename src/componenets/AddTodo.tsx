087cd12600c8ea1abc33aab7c58d577a5de69fd4
Hritik <pypi.pioneer@gmail.com>
2024-05-10 18:29:54 +0530

    form data fetched, now, make the api call and add a new todo

--- src/componenets/AddTodo.tsx
+++ src/componenets/AddTodo.tsx
@@ -6,20 +6,32 @@ import { Link, useNavigate } from "react-router-dom";
 // form to add todo
 const AddToDo: React.FC = () => {
 
-    // handle the prev page 
+    // to navigate to pages
     const navigate = useNavigate();
+
+    // handle the prev page 
     const goToPrevPage = () => {
         navigate(-1);
     }
 
+    // to handle the form submission
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // fetch the textarea data
+        const formData = new FormData(e.currentTarget);
+        const textareaValue = formData.get("todo") as string;
+
+        console.log(textareaValue)
+    }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="container">
                     <h1 className="mt-5 mb-4 text-center">Add Your Task</h1>
                     <div className="mb-3">
-                        <textarea className="form-control" rows={6}></textarea>
+                        <textarea name="todo" className="form-control" rows={6}></textarea>
                     </div>
 
                     <div className="d-flex justify-content-between">
